5777e17329281bfc534308f3ad285fb039ac306f
Akshay <akshaykagrawal7@gmail.com>
2017-07-21 22:09:34 -0700

    update index

--- generate_index.py
+++ generate_index.py
@@ -3,26 +3,27 @@ import glob
 import re
 import pdb
 
-META_EXT= '.meta'
-SUM_WILDCARD = 'summaries/*'
+PDF_META = 'PDF'
+MD_WILDCARD = 'summaries/*.md'
 HTML_WILDCARD = 'html/*.html'
 TITLE_PATTERN = r'^# \[(.*?) \(([a-zA-z\']*) ([0-9]{4})\)'
 
-sum_files = sorted(glob.glob(SUM_WILDCARD))
+md_files = sorted(glob.glob(MD_WILDCARD))
 html_files = sorted(glob.glob(HTML_WILDCARD))
 
 ListItem = collections.namedtuple('ListItem', ['title', 'author', 'year', 'url'])
 list_items = []
 
-for sum_file, html_file in zip(sum_files, html_files):
-    with open(sum_file, 'rb') as f:
+for md_file, html_file in zip(md_files, html_files):
+    with open(md_file, 'rb') as f:
         title_line = f.readline()
         match = re.search(TITLE_PATTERN, title_line)
         title = match.group(1)
         author = match.group(2)
         year = int(match.group(3))
-        if sum_file.endswith(META_EXT):
-            basename = sum_file.split('summaries/')[1]
+        meta = f.readline()
+        if meta == PDF_META:
+            basename = md_file.split('summaries/')[1]
             url = 'pdf/' + basename.split(META_EXT)[0] + '.pdf'
         else:
             url = html_file
